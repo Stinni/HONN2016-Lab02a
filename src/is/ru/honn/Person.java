7a8c726fe2b9a2de46806dc5b28ee52d33bbe983
Stinni <kristinnf13@ru.is>
2016-09-03 20:44:41 +0000

    Initial commit

---
+++ src/is/ru/honn/Person.java
@@ -0,0 +1,43 @@
+package is.ru.honn;
+
+import java.util.Date;
+
+/**
+ * Klasinn Person (Person.java)
+ * TODO: Add description
+ *
+ * @author Kristinn Heiðar Freysteinsson
+ * @version 1, 03.09.16
+ */
+public class Person {
+
+    protected String name;
+    protected Date dateOfBirth;
+
+    public Person(){}
+
+    public Person(String name, Date dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString() {
+        return "Person: " + getName();
+    }
+}
